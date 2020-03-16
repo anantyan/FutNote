0c1113583c829f1791286224c9ec19d04174e0ed
anantyan <anantya4995@gmail.com>
2020-03-16 10:49:04 +0700

    Second upload - add NoteDao, NoteDatabase from class

---
+++ app/src/main/java/com/futnote/mobile/components/NoteDao.java
@@ -0,0 +1,29 @@
+package com.futnote.mobile.components;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface NoteDao {
+
+    @Insert
+    void insert(Note note);
+
+    @Update
+    void update(Note note);
+
+    @Delete
+    void delete(Note note);
+
+    @Query("DELETE FROM tblnote")
+    void deleteAll();
+
+    @Query("SELECT * FROM tblNote ORDER BY id DESC")
+    LiveData<List<Note>> select();
+}

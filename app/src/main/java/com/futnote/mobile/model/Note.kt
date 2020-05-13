70a9436adadd9ac370788a9275c6b2fe2b44570a
Arya Rezza Anantya <43742778+anantyan@users.noreply.github.com>
2020-05-13 15:37:05 +0700

    Parcelable Model

--- app/src/main/java/com/futnote/mobile/model/Note.kt
+++ app/src/main/java/com/futnote/mobile/model/Note.kt
@@ -1,5 +1,7 @@
 package com.futnote.mobile.model
 
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -7,7 +9,35 @@ import androidx.room.PrimaryKey
 class Note(
         var judul: String,
         var deskripsi: String,
-        var prioritas: Int) {
+        var prioritas: Int): Parcelable {
     @PrimaryKey(autoGenerate = true)
     var id: Int = 0
+
+    constructor(parcel: Parcel) : this(
+            parcel.readString().toString(),
+            parcel.readString().toString(),
+            parcel.readInt()) {
+        id = parcel.readInt()
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(judul)
+        parcel.writeString(deskripsi)
+        parcel.writeInt(prioritas)
+        parcel.writeInt(id)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Note> {
+        override fun createFromParcel(parcel: Parcel): Note {
+            return Note(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Note?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
